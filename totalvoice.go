00e22888e5b8b2a620beb8737cf47ed4b755b533
dilowagner <diegowagner4@gmail.com>
2017-11-24 13:22:01 -0200

    Enviando struct Response

--- totalvoice.go
+++ totalvoice.go
@@ -15,6 +15,15 @@ type HTTPClient interface {
 	GetBaseURI() string
 }
 
+// TvceResponse -
+type TvceResponse struct {
+	Status   int    `json:"status"`
+	Sucesso  bool   `json:"sucesso"`
+	Motivo   int    `json:"motivo"`
+	Mensagem string `json:"mensagem"`
+	Dados    interface{}
+}
+
 // TotalVoice struct
 type TotalVoice struct {
 	client HTTPClient
