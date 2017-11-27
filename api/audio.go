3f8fb737b7a1d7002d71e48ac5940ecfd5f79036
dilowagner <diegowagner4@gmail.com>
2017-11-27 13:29:16 -0200

    Alterado metodo Enviar

--- api/audio.go
+++ api/audio.go
@@ -20,21 +20,21 @@ type Audio struct {
 	Mensagem string `json:"mensagem"`
 	Dados    struct {
 		ID                     int    `json:"id"`
-		NumeroDestino          string `json:"numero_destino"`
-		DataCriacao            string `json:"data_criacao"`
-		DataInicio             string `json:"data_inicio"`
-		Tipo                   string `json:"tipo"`
-		Status                 string `json:"status"`
-		DuracaoSegundos        int    `json:"duracao_segundos"`
-		Duracao                string `json:"duracao"`
-		DuracaoCobradaSegundos int    `json:"duracao_cobrada_segundos"`
-		DuracaoCobrada         string `json:"duracao_cobrada"`
-		DuracaoFaladaSegundos  int    `json:"duracao_falada_segundos"`
-		DuracaoFalada          string `json:"duracao_falada"`
-		Preco                  int    `json:"preco"`
-		URLAudio               string `json:"url_audio"`
-		RespostaUsuario        bool   `json:"resposta_usuario"`
-		Resposta               string `json:"resposta"`
+		NumeroDestino          string `json:"numero_destino,omitempty"`
+		DataCriacao            string `json:"data_criacao,omitempty"`
+		DataInicio             string `json:"data_inicio,omitempty"`
+		Tipo                   string `json:"tipo,omitempty"`
+		Status                 string `json:"status,omitempty"`
+		DuracaoSegundos        int    `json:"duracao_segundos,omitempty"`
+		Duracao                string `json:"duracao,omitempty"`
+		DuracaoCobradaSegundos int    `json:"duracao_cobrada_segundos,omitempty"`
+		DuracaoCobrada         string `json:"duracao_cobrada,omitempty"`
+		DuracaoFaladaSegundos  int    `json:"duracao_falada_segundos,omitempty"`
+		DuracaoFalada          string `json:"duracao_falada,omitempty"`
+		Preco                  int    `json:"preco,omitempty"`
+		URLAudio               string `json:"url_audio,omitempty"`
+		RespostaUsuario        bool   `json:"resposta_usuario,omitempty"`
+		Resposta               string `json:"resposta,omitempty"`
 	} `json:"dados"`
 }
 
@@ -44,7 +44,7 @@ type AudioService struct {
 }
 
 // Enviar - Envia uma mensagem de audio
-func (s AudioService) Enviar(numero string, urlAudio string, respostaUsuario bool, bina string) (string, error) {
+func (s AudioService) Enviar(numero string, urlAudio string, respostaUsuario bool, bina string) (*Audio, error) {
 
 	params := map[string]string{
 		"numero_destino":   numero,
@@ -53,8 +53,9 @@ func (s AudioService) Enviar(numero string, urlAudio string, respostaUsuario boo
 		"bina":             bina,
 	}
 
-	err := s.Client.CreateResource(params, RotaAudio)
-	return "", err
+	resp := new(Audio)
+	err := s.Client.CreateResource(params, RotaAudio, resp)
+	return resp, err
 }
 
 // BuscaAudio - Busca uma mensagem de audio pelo seu ID
