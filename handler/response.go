eb3bc4d02a1d655cddc9afc13343cd16253c7d02
dilowagner <diegowagner4@gmail.com>
2017-11-27 13:30:17 -0200

    Alterado Response padrão

--- handler/response.go
+++ handler/response.go
@@ -2,9 +2,9 @@ package handler
 
 // TvceResponse -
 type TvceResponse struct {
-	Status   int                 `json:"status"`
-	Sucesso  bool                `json:"sucesso"`
-	Motivo   int                 `json:"motivo"`
-	Mensagem string              `json:"mensagem"`
-	Dados    map[string]struct{} `json:"dados"`
+	Status   int      `json:"status"`
+	Sucesso  bool     `json:"sucesso"`
+	Motivo   int      `json:"motivo"`
+	Mensagem string   `json:"mensagem"`
+	Dados    struct{} `json:"dados"`
 }
